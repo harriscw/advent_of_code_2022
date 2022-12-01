aeb2fd748055f29a62c10c8316b287e7138cc52f
harriscw <harriscw@ucla.edu>
2022-12-01 05:07:58 -0800

    dont use a list

--- day01/py/d1.py
+++ day01/py/d1.py
@@ -2,14 +2,14 @@ text_file = open("../input.txt", "r")
 lines = [v.strip("\n") for v in text_file.readlines()]
 
 sumlist=[]
-acclist=[]
+acc=0
 
 for i,line in enumerate(lines):
-    if line != "" or i==len(lines): #you're not at the end of the elf, so append to list
-        acclist.append(int(line))
+    if line != "" or i==len(lines): #you're not at the end of the elf, so keep summing
+        acc += int(line)
     else: #you're at the end of the elf, so sum the list, append it, and reset it
-        sumlist.append(sum(acclist))
-        acclist=[]
+        sumlist.append(acc)
+        acc=0
 
 print("Part 1:",max(sumlist))
 sumlist.sort()
