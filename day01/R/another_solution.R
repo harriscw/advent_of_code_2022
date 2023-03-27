c65425e8cd5125b43de4c48e5326a4ea11cdf3fb
harriscw <harriscw@ucla.edu>
2023-03-27 15:57:00 -0700

    d1 tweak

--- day01/R/another_solution.R
+++ day01/R/another_solution.R
@@ -8,7 +8,7 @@ res=paste(input,collapse="+")
 res=unlist(strsplit(res,"\\+\\+"))
 
 #evaluate each string in the vector
-res = sapply(res,function(x){eval(parse(text=x))},simplify=TRUE)
+res = sapply(parse(text=res),eval,simplify=TRUE)
 
 paste("Part 1:",tail(sort(res),1))
 paste("Part 2:",sum(tail(sort(res),3)))
